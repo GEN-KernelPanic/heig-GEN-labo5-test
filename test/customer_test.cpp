e2c336386a5383dca37ad2dcf996018d0776e2ce
ClarFleu <clarisse.fleu@gmail.com>
2020-06-21 21:35:06 +0200

    Add correction

--- test/customer_test.cpp
+++ test/customer_test.cpp
@@ -7,4 +7,14 @@
  Remark(s)   :
  -----------------------------------------------------------------------------------
 */
+#include "gtest/gtest.h"
+#include "Customer.h"
 
+TEST(CustomerTest, StatementTest) {
+Customer customer("Jaques");
+
+customer.addRental(Rental(Movie("Rrrrrrrrrrrrrs"), 4));
+
+ASSERT_EQ(customer.statement(),
+"Rental Record for Jaques\n\tRrrrrrrrrrrrrs\t5\nAmount owed is 5\nYou earned 1 frequent renter points");
+}
