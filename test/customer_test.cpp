228d895f03f9f868948560cfe2164148f9e2a2fc
ClarFleu <clarisse.fleu@gmail.com>
2020-06-21 21:45:36 +0200

    Refactor test

--- test/customer_test.cpp
+++ test/customer_test.cpp
@@ -11,10 +11,10 @@
 #include "Customer.h"
 
 TEST(CustomerTest, StatementTest) {
-Customer customer("Jaques");
+    Customer customer("Jaques");
 
-customer.addRental(Rental(Movie("Rrrrrrrrrrrrrs"), 4));
+    customer.addRental(Rental(Movie("Rrrrrrrrrrrrrs"), 4));
 
-ASSERT_EQ(customer.statement(),
-"Rental Record for Jaques\n\tRrrrrrrrrrrrrs\t5\nAmount owed is 5\nYou earned 1 frequent renter points");
+    ASSERT_EQ(customer.statement(),
+    "Rental Record for Jaques\n\tRrrrrrrrrrrrrs\t5\nAmount owed is 5\nYou earned 1 frequent renter points");
 }
